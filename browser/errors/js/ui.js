ec00d7182b595af11f0d7a352abfcc096bb07576
A-Krivoshein <a.krivoshein@gmail.com>
2017-12-25 22:46:36 +0300

    first homework

--- browser/errors/js/ui.js
+++ browser/errors/js/ui.js
@@ -1,6 +1,26 @@
 'use strict';
-function handleClick {
-  openMail();
+openMail();
+function openMail() {
+  const values = ['Открыть', 'Закрыть'];
+  const btn = document.getElementById('open');
+  const mail = document.getElementById('mail');
+  var state = 1;
+  
+  mail.classList.toggle('is-active'); 
+  
+  changeText();
+
+  
+  btn.onclick = function revertMail(){
+    mail.classList.toggle('is-active'); 
+    state = mail.classList.contains('is-active') ? 1 : 0; 
+    
+    changeText(state);
+  };
+  function changeText(){
+    setTimeout(() => {
+      btn.value = values[state];
+    }, state * 2000);
+  }
 }
-const button = window.getElementById('open');
-button.onclick = handleClick();
+
